0148257dea53c38aab18510f3adf52cfa819ebf9
anilparikipandla <anilparikipandla@gmail.com>
2022-02-03 16:55:05 +0530

    reactUdemySection8: practise app - allconcepts covered so far

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
+
+function App() {
+  const [usersList, setUsersList] = useState([
+    {
+      id: Math.random().toString(),
+      name: 'John Smith',
+      age: 32,
+    },
+    {
+      id: Math.random().toString(),
+      name: 'John Doe',
+      age: 23,
+    },
+  ]);
+
+  const addUserHandler = (name, age) => {
+    setUsersList((prevUsersList) => {
+      return [...prevUsersList, { id: Math.random().toString(), name, age }];
+    });
+  };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/UI/ErrorModal.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Button from './Button';
+import Card from './Card';
+import styles from './ErrorModal.module.css';
+
+const ErrorModal = (props) => {
+  return (
+    <div>
+      <div className={styles.backdrop} onClick={props.onDismiss}></div>
+      <Card className={styles.modal}>
+        <header className={styles.header}>{props.title}</header>
+        <div className={styles.content}>
+          <p>{props.message}</p>
+        </div>
+        <footer className={styles.actions}>
+          <Button onClick={props.onDismiss}>Okay</Button>
+        </footer>
+      </Card>
+    </div>
+  );
+};
+
+export default ErrorModal;
