4c2d049cd75180661e635fa1f343fdd030102154
anilparikipandla <anilparikipandla@gmail.com>
2022-02-04 20:17:19 +0530

    reactUdemySection9: -Wrapper -Fragment -Portal -useRefs

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -2,21 +2,42 @@ import React from 'react';
 import Button from './Button';
 import Card from './Card';
 import styles from './ErrorModal.module.css';
+import ReactDOM from 'react-dom';
+
+const BackDrop = (props) => {
+  return <div className={styles.backdrop} onClick={props.onDismiss}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>{props.title}</header>
+      <div className={styles.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button onClick={props.onDismiss}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
 
 const ErrorModal = (props) => {
   return (
-    <div>
-      <div className={styles.backdrop} onClick={props.onDismiss}></div>
-      <Card className={styles.modal}>
-        <header className={styles.header}>{props.title}</header>
-        <div className={styles.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button onClick={props.onDismiss}>Okay</Button>
-        </footer>
-      </Card>
-    </div>
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <BackDrop onDismiss={props.onDismiss} />,
+        document.getElementById('backdrop-root')
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onDismiss={props.onDismiss}
+        />,
+        document.getElementById('overlay-root')
+      )}
+    </React.Fragment>
   );
 };
 
